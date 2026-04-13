a28834c87ceccb18a6a4883adc30142e3dfca8f4
BitsolutionCompany <166075575+BitsolutionCompany@users.noreply.github.com>
2026-04-13 20:19:06 -0300

    Creatung components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <h3>Header</h3>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main(){
+    return(
+        <main className="main">
+            <h3>Main</h3>
+        </main>
+    )
+}
