c2163cbda5c093339325fa8bdfb26d81cc66e5af
BitsolutionCompany <166075575+BitsolutionCompany@users.noreply.github.com>
2026-04-22 21:29:56 -0300

    footer bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "./styles/Footer.components.css"
 export default function Footer(){
     return (
         <footer className="footer">
-            <h3>Footer</h3>
+            <h3></h3>
         </footer>
     )
 }
